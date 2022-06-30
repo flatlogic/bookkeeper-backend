fbec70b6bb2dcee132ec2a43566da5ed2397a5bc
Solant <runner62v6@gmail.com>
2022-06-30 15:03:26 +0300

    fix: use newer credentials for postgres database

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,30 +1,7 @@
 import { createConnection, EntitySchema, getManager, ObjectType, Repository } from "typeorm";
 
-export const init = async () => {
-  const host: string = process.env.PGHOST;
-  const database: string = process.env.PGDATABASE;
-  const username: string = process.env.PGUSER;
-  const password: string = process.env.PGPASSWORD;
-  const port: number = +process.env.PGPORT;
-
-  try {
-    return await createConnection({
-      name: "bookkeeper_seed",
-      type: "postgres",
-      database,
-      host,
-      port,
-      username,
-      password,
-      extra: { ssl: true, rejectUnauthorized: false },
-      entities: [
-        __dirname + "/../models/*"
-      ],
-      synchronize: true,
-    });
-  } catch (e) {
-    console.error("DB connection issue: ", e);
-  }
+export const init = () => {
+    return createConnection();
 };
 
 export const getRepository = async <Entity>(
